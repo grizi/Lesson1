d02eadf8f2e914adb3ec0cf24ab265c1dbe46fcf
Stefan Voljc <stefan.voljc@gmail.com>
2015-10-14 13:08:53 +0200

    fibonacci iterativno

---
+++ naloga1.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var stevec:integer_t = 5
+
+
+func fiabonacci_iter(stevec:integer_t)->integer_t
+{
+    var prvi:integer_t  = 0
+    var drugi:integer_t = 1
+    var sestevek:integer_t = 0
+    
+    print(sestevek)
+    print(drugi)
+    for _ in 1...stevec
+    {
+        sestevek = prvi + drugi
+        print(String(sestevek)+" ")
+        prvi = drugi
+        drugi = sestevek
+        
+    }
+    return sestevek
+}
+
+print(fiabonacci_iter(stevec))
